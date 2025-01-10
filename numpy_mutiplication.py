1e68db05780f7b2ffa468e64cd16768113d15f89
Branis333 <b.sumba@alustudent.com>
2025-01-10 13:00:46 +0200

    updated matrix multiplication with numpy

--- numpy_mutiplication.py
+++ numpy_mutiplication.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 def get_matrix_from_user():
+    
     rows = int(input("Enter the number of rows: "))
     cols = int(input("Enter the number of columns: "))
     print(f"Enter the elements of the matrix ({rows}x{cols}), row by row:")
@@ -17,22 +18,3 @@ def matrix_multiply(matrix1, matrix2):
         return np.dot(matrix1, matrix2)
     except ValueError as e:
         raise ValueError(f"Matrix multiplication failed: {e}")
-
-if __name__ == "__main__":
-    print("Input the first matrix:")
-    matrix1 = get_matrix_from_user()
-    print("Input the second matrix:")
-    matrix2 = get_matrix_from_user()
-    
-    try:
-        print("Matrix 1:")
-        print(matrix1)
-        print("Matrix 2:")
-        print(matrix2)
-        
-        result = matrix_multiply(matrix1, matrix2)
-        print("Result of matrix multiplication:")
-        print(result)
-    except ValueError as e:
-        print(e)
-
