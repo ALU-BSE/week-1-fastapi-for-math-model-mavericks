82a0450e79ba1158ed2f326f751275ade4b947c0
gyhaan <g.yhaan@alustudent.com>
2025-01-10 17:49:34 +0200

    added sigmoid function

--- plain_multiplication.py
+++ plain_multiplication.py
@@ -1,3 +1,13 @@
+M = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10]
+]
+
+B = [
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+]
+
 def plain_mat_mul(X):
     # Create result matrix
     MX = [[0] * len(X[0]) for _ in range(len(M))]
