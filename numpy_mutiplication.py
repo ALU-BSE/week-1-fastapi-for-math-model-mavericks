6581d70f076c7745d3dec83335115ab986e3ed7e
Christian-TS <christian.byiringiro.sp@dlh.de>
2025-01-10 16:02:19 +0200

    implement computation without numpy

--- numpy_mutiplication.py
+++ numpy_mutiplication.py
@@ -1,5 +1,15 @@
 import numpy as np
 
+M = [
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10]
+]
+
+B = [
+    [11, 12, 13, 14, 15],
+    [16, 17, 18, 19, 20]
+]
+
 def get_matrix_from_user():
     
     rows = int(input("Enter the number of rows: "))
@@ -18,3 +28,22 @@ def matrix_multiply(matrix1, matrix2):
         return np.dot(matrix1, matrix2)
     except ValueError as e:
         raise ValueError(f"Matrix multiplication failed: {e}")
+
+
+def plain_mat_mul(X):
+    # Create result matrix
+    MX = [[0] for _ in len(X[0]) for _ in len(M)]
+
+    # Do multiplication (MX)
+    for i in range(len(M)):
+        for j in range(len(X[0])):
+            for k in range(len(X)):
+                MX[i][j] += M[i][k] * X[k][j]
+    
+    # Do addition ( + B)
+    Y = [
+        [MX[i][j] + B[i][j] for j in range(len(MX[0]))] for i in range(MX)
+    ]
+
+    # return
+    return Y
